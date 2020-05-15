f04efbf78d85abb4800f595b21d63c61186ee802
beccatembel <65377858+beccatembel@users.noreply.github.com>
2020-05-14 20:51:32 -0500

    uploading second homework2020

---
+++ MyName.java
@@ -0,0 +1,8 @@
+public class MyName{
+	public static void main(String[]args){
+
+		String myName= "Becca";
+		String lastName="Sultan";
+		 System.out.println("My first name is "+myName+" and my last name is "+lastName);
+	}
+}
